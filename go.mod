2ce4fdbd84eeed0d3023f899e2df888cb937751d
Rafael R. de Oliveira <consultoria@tekplus.com.br>
2023-04-09 15:25:06 -0300

    entities created and tested

--- go.mod
+++ go.mod
@@ -7,10 +7,15 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.1
+	gorm.io/driver/sqlite v1.4.4
+	gorm.io/gorm v1.24.6
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
 
---
+++ internal/entity/infra/database/interfaces.go
@@ -0,0 +1,16 @@
+package database
+
+import "github.com/rafagitaccount/api_go/internal/entity"
+
+type UserIterface interface {
+	Create(user *entity.User) error
+	FindByEmail(email string) (*entity.User, error)
+}
+
+type ProductIterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) (*[]entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
