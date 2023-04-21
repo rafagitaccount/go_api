c58c4f13e896be03ea00ed39502ba6a1849507ff
Rafael R. de Oliveira <consultoria@tekplus.com.br>
2023-04-21 17:52:14 -0300

    feature: jwt token middleware added to products endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
+	"github.com/rafagitaccount/api_go/configs"
+	"github.com/rafagitaccount/api_go/internal/entity"
+	"github.com/rafagitaccount/api_go/internal/infra/database"
+	"github.com/rafagitaccount/api_go/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("teste.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDb := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDb, configs.TokenAuth, configs.JWTExpiresIn)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	http.ListenAndServe(":8000", r)
 
 }
